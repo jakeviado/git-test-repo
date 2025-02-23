8c789899e0ad7e4ee2e4c9989cd001045b48ba84
jakeeviado <jakematthewviado204@gmail.com>
2025-02-24 00:34:26 +0800

    text based feature

--- src/main/java/com/demo/oop/application/Application.java
+++ src/main/java/com/demo/oop/application/Application.java
@@ -1,20 +1,35 @@
 package com.demo.oop.application;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Select: ");
+        System.out.println("1. Circle ");
+        System.out.println("2: Ellipse ");
 
-        System.out.println("Circle");
-        Circle circle = new Circle();
+        int choice = sc.nextInt();;
+        switch (choice) {
+            case 1:
+                System.out.println("Circle");
+                Circle circle = new Circle();
 
-        circle.getCircumference();
-        circle.getArea();
+                circle.getCircumference();
+                circle.getArea();
 
+                break;
 
-        System.out.println("\n" + "Ellipse");
+            case 2:
+                System.out.println("\n" + "Ellipse");
 
-        Ellipse ellipse = new Ellipse();
+                Ellipse ellipse = new Ellipse();
 
-        ellipse.getCircumference();
-        ellipse.getArea();
+                ellipse.getCircumference();
+                ellipse.getArea();
+
+                break;
+
+        }
     }
 }
